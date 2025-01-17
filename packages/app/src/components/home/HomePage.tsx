30e764db18047c797bd7d7a004edb387ebb44e8c
Felipe Farias <felipe.silva@intelipost.com.br>
2025-01-17 11:52:43 -0300

    feat: improvement greetings at home page

--- packages/app/src/components/home/HomePage.tsx
+++ packages/app/src/components/home/HomePage.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid } from '@material-ui/core';
 import { InfoCard } from '@backstage/core-components';
-
+import { useApi, identityApiRef, ProfileInfo } from '@backstage/core-plugin-api';
 import { AnnouncementsTimeline } from '@backstage-community/plugin-announcements';
 
 import {
@@ -12,9 +12,27 @@ import {
 
 const HomePage = () => {
 
+  const identityApi = useApi(identityApiRef);
+  const [profile, setProfile] = useState<ProfileInfo | null>(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const userProfile = await identityApi.getProfileInfo();
+      setProfile(userProfile);
+    };
+
+    fetchProfile();
+  }, [identityApi]);
+
+  if (!profile) {
+    return <div>Carregando...</div>; // Exibe algo enquanto carrega
+  }
+
   return (
     <Page themeId="home">
-      <Header title="Home" />
+      <Header 
+        title={`Seja bem-vindo${profile ? `, ${profile.displayName} 👋` : ''}`} 
+      />
       <Content>
         <h1>Announcements Timeline</h1>
         <Grid container>
