77652a1f7bfcd5099b9d0c4dea7431917009452d
Felipe Farias <felipe.silva@intelipost.com.br>
2025-01-17 08:46:56 -0300

    feat: added argocd plugin

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -59,6 +59,11 @@ import {
   isKubernetesAvailable,
 } from '@backstage/plugin-kubernetes';
 
+import {
+  EntityArgoCDOverviewCard,
+  isArgocdAvailable
+} from '@roadiehq/backstage-plugin-argo-cd';
+
 const techdocsContent = (
   <EntityTechdocsContent>
     <TechDocsAddons>
@@ -142,6 +147,15 @@ const overviewContent = (
     <Grid item md={8} xs={12}>
       <EntityHasSubcomponentsCard variant="gridItem" />
     </Grid>
+
+    <EntitySwitch>
+      <EntitySwitch.Case if={e => Boolean(isArgocdAvailable(e))}>
+        <Grid item sm={4}>
+          <EntityArgoCDOverviewCard />
+        </Grid>
+      </EntitySwitch.Case>
+    </EntitySwitch>
+    
   </Grid>
 );
 
